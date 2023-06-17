df3578d2816d3b4e8734a660021670975521fad4
vinodkumarme117 <vinodkumarme117@gmail.com>
2023-06-17 17:18:02 +0530

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -29,8 +29,7 @@ module.exports = defineConfig({
   
   e2e: {
     setupNodeEvents(on, config) {
-      
-      // implement node event listeners here
+            // implement node event listeners here
     },
     setupNodeEvents,
     //baseURL:'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login',
